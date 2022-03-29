b15a73156fe44fd408b4195c0eb932e857003c88
da631sch <da631sch@htwg-konstanz.de>
2022-03-29 16:47:18 +0200

    new comments

--- Aufgabe 1/SupermarktTest.py
+++ Aufgabe 1/SupermarktTest.py
@@ -1,1 +1,35 @@
+
+import heapq as hq
+
 print("Hello World")
+
+#Methode start() ##startet sim
+
+
+heap = []
+
+for i in range(10):
+    heap.append(i)
+    print(heap)
+hq.heapify(heap)
+print(heap)
+#Kl     Ereignisliste
+#   KV:     Heap-Queue: Simulationszeit und die Ereignisnummer
+#   KM:     event=pop(), push(event) und start()
+#
+
+#KL:    Station
+
+#Heap-Queue: z.B (1,2,3,4,5)    Ereigniszeitpunkt, Ereignispriorität,
+#                               Ereignisnummer,  Ereignisfunktion
+#                               und optional den Ereignisargumenten
+
+#KL:    KundeIN
+#       Konstrukter --> 3Tupel (zu besuchende Stationen)
+#       L2=list(L1)
+
+
+
+
+
+
